1d908ef5c7c72d013ebf28bc281eb348fd4e1447
Wesley Ng <wesley@docustream.com>
2017-05-25 16:17:55 -0500

    Initial Commit

---
+++ FizzBuzzGame/FizzBuzzRulesClassModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzGame
+{
+    public class FizzBuzzRulesClassModel
+    {
+        public int ModuloNumber { get; set; }
+        public string ReplacementWord { get; set; }
+        public FizzBuzzRulesClassModel(int _moduloNumber, string _replacementWord)
+        {
+            this.ModuloNumber = _moduloNumber;
+            this.ReplacementWord = _replacementWord;
+        }
+    }
+}
---
+++ UnitTestProject1/ModelTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FizzBuzzGame;
+
+namespace UnitTestProject1
+{
+    /// <summary>
+    /// Summary description for ModelTest
+    /// </summary>
+    [TestClass]
+    public class ModelTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            FizzBuzzRulesClassModel m = new FizzBuzzRulesClassModel(8, "potato");
+
+            Assert.AreEqual(m.ModuloNumber, 8);
+            Assert.AreEqual(m.ReplacementWord, "potato");
+        }
+    }
+}
